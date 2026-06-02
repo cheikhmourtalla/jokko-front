8f01aa64512a18f9494636d5696875c3a371aa54
mouha080 <walkerkyleissue@gmail.com>
2026-06-02 04:16:18 +0000

    fix: correct all API URLs in SuperAdmin

--- src/services/index.ts
+++ src/services/index.ts
@@ -3,7 +3,7 @@
 // ============================================================
 import axios from "axios";
 
-export const api = axios.create({ baseURL: "http://localhost:5000/api" });
+export const api = axios.create({ baseURL: "http://${import.meta.env.VITE_API_URL}st:5000/api" });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
